94853140d0aa73517df338343e29c77c771d2667
André Siqueira <siqueira.andre@gmail.com>
2020-04-18 08:25:14 -0300

    ok

--- src/main/java/br/biblioteca/livros/service/AutorService.java
+++
@@ -1,8 +0,0 @@
-package br.bliblioteca.livros.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AutorService {
-
-}
--- src/main/java/br/bliblioteca/livros/controladores/AutorController.java
+++
@@ -1,8 +0,0 @@
-package br.bliblioteca.livros.controladores;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AutorController {
-
-}
