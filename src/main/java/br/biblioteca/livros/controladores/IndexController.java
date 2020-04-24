5593bf09352c63a816cba46beef66f22efc6442e
André Siqueira <siqueira.andre@gmail.com>
2020-04-24 07:05:42 -0300

    inclusao das avaliacoes nos livros

--- src/main/java/br/biblioteca/livros/controladores/IndexController.java
+++ src/main/java/br/biblioteca/livros/controladores/IndexController.java
@@ -14,19 +14,3 @@ public class IndexController {
 	}
 
 }
-package br.biblioteca.livros.controladores;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-@RequestMapping("/")
-public class IndexController {
-
-	public ModelAndView index() {
-		ModelAndView modelAndView = new ModelAndView("index");
-		return modelAndView;
-	}
-
-}
